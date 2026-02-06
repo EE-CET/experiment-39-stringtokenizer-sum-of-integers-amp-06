a8567364e87b95bdfc6690ebcb97b363eb0218c5
amrita-m-pillai <amritampillai06@gmail.com>
2026-02-06 21:50:18 +0530

    first commit

---
+++ src/Expt39.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Expt39 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(line);
+        int sum = 0;
+
+        while (st.hasMoreTokens()) {
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(sum);
+    }
+}
