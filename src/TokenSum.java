09af4feaa1ae401d95bcdd912b76ead70ff949a2
amrita-m-pillai <amritampillai06@gmail.com>
2026-02-07 01:50:26 +0530

    correction

--- src/TokenSum.java
+++ src/TokenSum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Expt39 {
+public class TokenSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
